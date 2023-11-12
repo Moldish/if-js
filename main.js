59e77d16fd53ebeb2e43230fd54164f7390d123e
Moldish <vlad569davydkin@gmail.com>
2023-11-12 14:44:04 +0300

    Fixed deepEqual function

--- main.js
+++ main.js
@@ -29,15 +29,30 @@ const obj3 = {
   b: 'b',
 };
 
-const deepEqual = (object1, object2) => {
-  for (const x in object1) {
-    if (Object.prototype.hasOwnProperty.call(object1, x) !== Object.prototype.hasOwnProperty.call(object2, x)) return false;
-    if (typeof object1[x] == 'object') {
-      if (!deepEqual(object1[x], object2[x])) return false;
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (obj1 == null || typeof obj1 !== "object" || obj2 == null || typeof obj2 !== "object") {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
     }
   }
+
   return true;
-};
+}
 
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
