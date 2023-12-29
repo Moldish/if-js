a4a0f92f1150fe4b3f6a8dbeb282fb99ac82dc37
Moldis <vlad569davydkin@gmail.com>
2023-12-29 10:06:56 +0300

    added new section 'Available hotels'. list of available hotels pushing to section Available hotels when button "search" clicked using search query from input "City" using fetch API

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ import './sections/homes';
 import './sections/hotel-data';
 import './sections/main-calendar';
 import './sections/main-persons-filter';
+import './sections/available-hotels';
 import 'bootstrap/js/index.esm';
